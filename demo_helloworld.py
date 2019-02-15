67d7a88bcbf9632b36ed38e045c7c0da82f7a0ae
itsmyrepository <47660353+itsmyrepository@users.noreply.github.com>
2019-02-15 18:03:01 +0800

    Add files via upload

---
+++ demo_helloworld.py
@@ -0,0 +1,14 @@
+print("Enter your name:")
+name = input()
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(0,"orange")
+thistuple = ("tuple1","tuple2","tuple3")
+thisset = {"set1","set2","set3"}
+#thistuple[1] = "tuple4"
+print(thistuple)
+print(thisset)
+# thislist.clear()
+# for x in thislist:
+  # print(x)
+  # print(thislist)
+print("Hello, " + name)
